eb92cd0e1533609b5e2eb2a8ac753840791a080e
Administrator <Administrator@DESKTOP-868OIQ9>
2021-12-29 13:48:07 +0100

    Add project files.

---
+++ ProfessionalLesson10Task1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ProfessionalLesson10Task1
+{
+    //Реализуйте шаблон NVI в собственной иерархии наследования.
+    class BaseClass
+    {
+        public void NVIMeth()
+        {
+            First();
+            Second();
+        }
+        protected virtual void First() => Console.WriteLine("First Base");
+        protected virtual void Second() => Console.WriteLine("Second Base");
+    }
+
+    class DerivedClass : BaseClass
+    {
+        protected sealed override void First() => Console.WriteLine("First Derived");
+        protected sealed override void Second() => Console.WriteLine("Second Derived");
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseClass bc = new DerivedClass();
+            bc.NVIMeth();
+        }
+    }
+}
---
+++ ProfessionalLesson10Task2/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace ProfessionalLesson10Task2
+{
+    //    Задание 2
+    //Выучите описание шаблона Template method(Шаблонный метод). Обратите внимание на
+    //применимость шаблона, а также на состав его участников и связи отношения между ними.
+    //Напишите небольшую программу на языке C#, представляющую собой абстрактную
+    //реализацию данного шаблона.
+
+    abstract class Programmer
+    {
+        public void Cycle(int? meth, bool all)
+        {
+            if (all == true)
+            {
+                DoWork();
+                DoHobby();
+                GetSalary();
+                return;
+            }
+            switch (meth)
+            {
+                case null:
+                    break;
+                case 1:
+                    DoWork();
+                    break;
+                case 2:
+                    DoHobby();
+                    break;
+                case 3:
+                    GetSalary();
+                    break;
+                default:
+                    break;
+            }
+        }
+        protected abstract void DoWork();
+        protected abstract void DoHobby();
+        protected abstract void GetSalary();
+
+    }
+
+    class CSProgrammer : Programmer
+    {
+        protected override void DoHobby() => Console.WriteLine("DO C# hobby");
+        protected override void DoWork() => Console.WriteLine("DO C# Work");
+        protected override void GetSalary() => Console.WriteLine("Get C# Salary");
+        protected void BuySmth() => Console.WriteLine("Buy Smth");
+    }
+
+    class JSPropgrammer : Programmer
+    {
+        protected override void DoHobby() => Console.WriteLine("DO JS hobby");
+        protected override void DoWork() => Console.WriteLine("DO JS Work");
+        protected override void GetSalary() => Console.WriteLine("Get JS Salary");
+        protected void PlayGame() => Console.WriteLine("Play GAMe");
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Programmer js = new JSPropgrammer();
+            Programmer cs = new JSPropgrammer();
+            Console.WriteLine($"JS Sycle");
+            js.Cycle(1, false);
+            Console.WriteLine("\n\n");
+            Console.WriteLine($"CS Sycle");
+            cs.Cycle(1, true);
+            Console.WriteLine("\n\n");
+
+        }
+    }
+}
